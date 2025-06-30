977c6c0f1565d97b3de7732ba6dda34e48f76057
Prince Maharjan <maharjanxprince@gmail.com>
2025-06-30 14:59:35 +0000

    Registration routing and error fixed

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from extensions import db
 from config import Config
 from flask import render_template, redirect, url_for, flash, request
 from forms import RegisterForm
-# from app import db
 
-db = SQLAlchemy()
+
 
 
 #build app
@@ -26,7 +25,35 @@ def create_app():
     def register():
         form = RegisterForm()
         if form.validate_on_submit():
-            return render_template("register.html", form = form)
+            # check for duplicates
+            duplicate = User.query.filter(
+                (User.username == form.username.data) |
+                (User.email == form.email.data)
+            ).first()
+            if duplicate:
+                flash("Username or email already registered", "danger")
+                return render_template("register.html", form = form)
+            
+            #Creating User
+            user = User(username = form.username.data,
+                        email = form.email.data)
+            user.set_password(form.password.data)
+
+            #to Database
+            db.session.add(user)
+            db.session.commit()
+
+            flash("Registration sucessful!", "sucess")
+            return redirect(url_for("home"))
+        
+#Get or validation erros fall through to here
+        return render_template("register.html", form = form)
+    
+    @app.route("/")
+    def home():
+        return "<h1>Welcome to FlaskBook!</h1>"
+
+    return app
 
 if __name__ == "__main__":
     app = create_app()
